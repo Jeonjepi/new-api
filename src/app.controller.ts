7784faab6b7df933ca6fef5fd47e280007105125
caitory <dev@caitory>
2022-03-15 17:48:46 +0900

    master

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,27 @@
-import { Controller, Get, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res, StreamableFile, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { Request, response, Response } from 'express';
+import { fileURLToPath } from 'url';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Post()
   @UseInterceptors(FilesInterceptor('image'))
   uploadFile(@UploadedFile() file, @Res() res){
     console.log(file)
-    return res.sendFile({root : __dirname});
+  }
+
+  @Get()
+  getFile(@Res() res: Response, @Req() req: Request){
+    const fileName = req.params.fileName;
+    const path = __dirname + "./uploads/"
+    return res.download("./uploads/" + fileName);
+  
   }
 
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express/multer';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [MulterModule.register({
     dest: './uploads'
-  })],
+  }),
+  ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'client'),
+    exclude: ['/api*'],
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
